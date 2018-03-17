2f5b9285c671ceae595b847946daa7c4749ec9ca
Matt Rochford <mattjohnrochford@gmail.com>
2018-03-16 22:59:57 -0700

    start switch to sql

---
+++ js/objects/sql.wrapper.js
@@ -0,0 +1,21 @@
+const sql = require('mysql');
+
+// DATE - format YYYY-MM-DD
+// DATETIME - format: YYYY-MM-DD HH:MI:SS
+// TIMESTAMP - format: YYYY-MM-DD HH:MI:SS
+// YEAR - format YYYY or YY
+
+// +-------+---------+------------+
+// | DogID | DogName | ClientName |
+// +-------+---------+------------+
+// |     1 | Blitzen | Weinstein  |
+// |     2 | Bruce   | Vacharis   |
+// +-------+---------+------------+
+
+class SQLInterface {
+
+
+
+}
+
+module.exports = SQLInterface;
