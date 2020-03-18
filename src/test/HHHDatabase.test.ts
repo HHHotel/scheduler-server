1df3e912f24ddab4c28b583f5781e6cb233a8f3d
Matt Rochford <mattjohnrochford@gmail.com>
2020-03-18 02:39:00 -0700

    New promised based api for qrys on database, better db column names

--- src/test/HHHDatabase.test.ts
+++ src/test/HHHDatabase.test.ts
@@ -1,11 +1,11 @@
 import assert = require("assert");
-import HHHDB = require("../HHHDatabase");
-import { DEFAULT, IHoundDog, IHoundAPIDog, IHoundAPIEvent } from "@happyhoundhotel/hounds-ts";
-const database = HHHDB.createDatabase(HHHDB.parseDatabaseString(process.env.CLEARDB_DATABASE_URL));
+import HoundsDatabase from "../HHHDatabase";
+// eslint-disable-next-line
+import * as hlib from "@happyhoundhotel/hounds-ts";
+const database = new HoundsDatabase(process.env.CLEARDB_DATABASE_URL);
 
 describe("HHH Database", () => {
-
-    const major: IHoundAPIDog = {
+    const major: hlib.IHoundAPIDog = {
         bookings: [
             {
                 desc: "",
@@ -25,162 +25,134 @@ describe("HHH Database", () => {
             },
 
         ],
-        clientName: "Johnson",
+        clientName: "Joseph Johnson",
         id: null,
-        name: "Major",
+        name: "Major J ZZZZ",
         activeClient: true,
     };
 
-    describe("#addDog()", () => { it("Shouldn't complain", (done) => {
-            HHHDB.addDog(database, major,
-                (results) => {
-                    assert.deepEqual(results.warningCount, 0);
-                    done();
-                });
+    describe("#addDog()", () => {
+        it("Shouldn't complain", async () => {
+            await database.addDog(major);
+            const res = await database.find(major.name);
+            const dog = res[0] as hlib.IHoundAPIDog;
+            assert.equal(major.name, dog.name);
+            major.id = dog.id;
         });
     });
 
     describe("#find()", () => {
-        it("Should find Major", (done) => {
-            const searchText: string = "Major";
-            HHHDB.find(database, searchText,
-                (results) => {
-                    const dog: IHoundAPIDog = results[0] as IHoundAPIDog;
-                    assert.deepEqual(dog.name + " " + dog.clientName, "Major Johnson");
-                    major.id = dog.id;
-                    done();
-                });
+        it("Should find Major", async () => {
+            const searchText: string = "major";
+            const res = await database.find(searchText);
+            const dog = res[0] as hlib.IHoundAPIDog;
+            assert.deepEqual(dog.id, major.id);
+        });
+
+        it("Should find Major from joseph", async () => {
+            const searchText: string = "joseph johnson";
+            const res = await database.find(searchText);
+            const dog = res[0] as hlib.IHoundAPIDog;
+            assert.deepEqual(dog.id, major.id);
         });
     });
 
     describe("#addEvent()", () => {
-        it("Shouldn't complain", (done) => {
-            let eventNum = major.bookings.length;
+        it("Shouldn't complain", async () => {
             for (const booking of major.bookings) {
                 booking.id = major.id;
-
-                HHHDB.addEvent(database, booking,
-                    (results) => {
-                        assert.deepEqual(results.warningCount, 0);
-                        if (--eventNum === 0) { done(); }
-                    });
+                await database.addEvent(booking);
             }
         });
     });
 
     describe("#addUser()", () => {
-        it("Should create a user", (done) => {
+        it("Should create a user", async () => {
             const user = {
                 password: "testing",
                 permissions: 0,
                 username: "testing",
             };
-            HHHDB.addUser(database, user.username, user.password, user.permissions, () => done());
+            await database.addUser(user);
         });
     });
 
     describe("#changePassword()", () => {
-        it("Should change a password", (done) => {
+        it("Should change a password", async () => {
             const user = {
                 password: "testing",
                 permissions: 0,
                 username: "testing",
             };
-            HHHDB.changePassword(database, user.username, user.password, "testingNew",
-                (res) => {
-                    assert.equal(res, "Success");
-                    done();
-                });
+            await database.changePassword(user.username, user.password, "testingNew");
         });
     });
 
     describe("#deleteUser()", () => {
-        it("Should delete the testing user", (done) => {
-            HHHDB.deleteUser(database, "testing");
-            done();
+        it("Should delete the testing user", () => {
+            database.deleteUser("testing");
         });
     });
 
     describe("#retrieveDog()", () => {
-        it("Proper retrieval of Major", (done) => {
-            HHHDB.retrieveDog(database, major.id, (res: IHoundAPIDog) => {
-                res.bookings.sort((a, b) => a.startDate - b.startDate);
-                major.bookings.sort((a, b) => a.startDate - b.startDate);
-                major.bookings.map((booking, i) => { booking.id = res.bookings[i].id; });
-                assert.deepEqual(res, major);
-                done();
-            });
-
+        it("Proper retrieval of Major", async () => {
+            const res = await database.retrieveDog(major.id);
+            res.bookings.sort((a, b) => a.startDate - b.startDate);
+            major.bookings.sort((a, b) => a.startDate - b.startDate);
+            major.bookings.map((booking, i) => booking.id = res.bookings[i].id);
+            assert.deepEqual(res, major);
         });
     });
 
     describe("#deleteDog", () => {
-        it("Should remove Major and booking", (done) => {
-            HHHDB.removeDog(database, major.id, () => {
-                HHHDB.find(database, "Major Johnson", (res) => {
-                    assert.deepEqual(res.length, 0);
-                    done();
-                });
-            });
+        it("Should remove Major and booking", async () => {
+            await database.removeDog(major.id);
+            const res = await database.find("Major Johnson");
+            assert.deepEqual(res.length, 0);
         });
     });
 
-    const groomingEvent: IHoundAPIEvent = {
+    const groomingEvent: hlib.IHoundAPIEvent = {
         desc: "",
         startDate: new Date("1/23/4567 8:09").valueOf(),
         endDate: new Date("1/23/4567 16:09").valueOf(),
         type: "grooming",
-        text: "Blitzen w/4P",
+        text: "Blitzen w/4P 123141fsjan-12312j1d34",
         id: "",
     };
 
     describe("#addEvent", () => {
-        it("Should add grooming", (done) => {
-            HHHDB.addEvent(database, groomingEvent, null);
+        it("Should add grooming", async () => {
+            await database.addEvent(groomingEvent);
 
-            HHHDB.find(database, groomingEvent.text, (res) => {
-                res.map((result) => {
-                    result = result as IHoundAPIEvent;
-                    if (result.startDate && result.startDate === groomingEvent.startDate) {
-                        done();
-                    }
-                });
+            const res = await database.find(groomingEvent.text);
+            res.map((result) => {
+                result = result as hlib.IHoundAPIEvent;
+                assert.equal(result.startDate, groomingEvent.startDate);
             });
-
         });
     });
 
     describe("#getWeek", () => {
-        it("should do things", (done) => {
-            HHHDB.getWeek(database, new Date("1/23/4567 8:09"), (week) => {
-                assert.equal(week[0].startDate, groomingEvent.startDate);
-                done();
-            });
+        it("should do things", async () => {
+            const week = await database.getWeek(new Date("1/23/4567 8:09"));
+            assert.equal(week[0].startDate, groomingEvent.startDate);
         });
     });
 
     describe("#removeEvent", () => {
-        it("Should remove grooming", (done) => {
-            HHHDB.find(database, groomingEvent.text, (res) => {
-                res.map((result) => {
-                    result = result as IHoundAPIEvent;
-                    if (result.startDate && result.startDate === groomingEvent.startDate) {
-                        remove(result.id);
-                    }
-                });
-
-                done();
-
-                function remove(id) {
-                    HHHDB.removeEvent(database, id, null);
+        it("Should remove grooming", async () => {
+            const res = await database.find(groomingEvent.text);
+            res.map((result) => {
+                result = result as hlib.IHoundAPIEvent;
+                if (result.startDate && result.startDate === groomingEvent.startDate) {
+                    database.removeEvent(result.id);
                 }
             });
         });
     });
 
     /* TODO
-       describe("#removeEvent", () => {
-       });
        describe("#editDog", () => {
        });
        describe("#editEvent", () => {
