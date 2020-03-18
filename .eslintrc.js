dbb0ac7e7074f16b21b97a09471a3c142584de8f
Matt Rochford <mattjohnrochford@gmail.com>
2020-03-18 02:35:58 -0700

    eslint config inherit from other projects gitignore some crud

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es6": true
@@ -10,12 +10,17 @@
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
+    "plugins": [
+        "@typescript-eslint"
+    ],
     "rules": {
-        "indent": ["error", 4],
-        "quotes": ["error", "double"]
+        "quotes": ["error", "double"],
+        "indent": ["warn", 4],
+        "max-len": ["warn", 100],
     }
-}
+};
